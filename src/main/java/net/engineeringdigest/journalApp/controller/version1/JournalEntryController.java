58de18f068026f3960beb141e76a4c02e1db4735
Aditya Saraswat <saraswataditya664@gmail.com>
2026-05-27 15:32:23 +0530

    add v1 journal controller

---
+++ src/main/java/net/engineeringdigest/journalApp/controller/version1/JournalEntryController.java
@@ -0,0 +1,40 @@
+package net.engineeringdigest.journalApp.controller.version1;
+
+import java.util.*;
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/_journal")
+public class JournalEntryController {
+    private Map<String, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
+
+    @GetMapping("/id/{myId}")
+    public JournalEntry getJournalEntryByID(@PathVariable String myId){
+        return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("/id/{myId}")
+    public JournalEntry DeleteJournalEntryByID(@PathVariable String myId){
+        return journalEntries.remove(myId);
+    }
+
+    @PutMapping("/id/{id}")
+    public JournalEntry UpdateJournalEntryByID(@PathVariable String id, @RequestBody JournalEntry myEntry){
+        return journalEntries.put(id, myEntry);
+    }
+}
