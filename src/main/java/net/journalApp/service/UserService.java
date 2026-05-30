e463a77cb12ba628ede1a1e731e51cfc3f5f3034
Aditya Saraswat <saraswataditya664@gmail.com>
2026-05-31 00:13:52 +0530

    changed user service

--- src/main/java/net/journalApp/service/UserService.java
+++ src/main/java/net/journalApp/service/UserService.java
@@ -5,10 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import net.journalApp.repository.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import net.journalApp.entity.User;
 
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,7 +22,15 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     public void saveEntry(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("USER"));
+        userRepository.save(user);
+    }
+
+    public void saveNewUser(User user){
         userRepository.save(user);
     }
 
