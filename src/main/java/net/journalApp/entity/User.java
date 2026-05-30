0da7f6b48ec828e924a44448da8f1bcb22cb3d47
Aditya Saraswat <saraswataditya664@gmail.com>
2026-05-30 08:51:57 +0530

    added noargs in user entity

--- src/main/java/net/journalApp/entity/User.java
+++ src/main/java/net/journalApp/entity/User.java
@@ -1,6 +1,7 @@
 package net.journalApp.entity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 
 @Document(collection = "users")
 @Data
+@NoArgsConstructor
 public class User {
 
     @Id
